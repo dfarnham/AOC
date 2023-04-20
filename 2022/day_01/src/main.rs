a20c33e5cdc5601dde642eed2411c40abc8ac77a
Dave Farnham <dave.farnham@icloud.com>
2023-04-20 10:29:24 -0600

    add itertools solution

--- 2022/day_01/src/main.rs
+++ 2022/day_01/src/main.rs
@@ -1,8 +1,25 @@
 use general::{get_args, read_trimmed_data_lines, reset_sigpipe};
 use std::error::Error;
 use std::io::{self, Write};
+use itertools::Itertools;
 
-fn count_calories(puzzle_lines: &[String], n: usize) -> Result<u64, Box<dyn Error>> {
+
+// itertools gem from https://fasterthanli.me/series/advent-of-code-2022/part-1
+fn count_calories<T>(puzzle_lines: &[String], n: usize) -> Result<T, Box<dyn Error>>
+where
+    T: std::str::FromStr + std::iter::Sum + std::cmp::Ord + std::fmt::Debug,
+{
+    Ok(puzzle_lines.iter()
+        .map(|s| s.parse::<T>().ok())
+        .batching(|it| it.map_while(|x| x).sum1::<T>())
+        .map(std::cmp::Reverse)
+        .k_smallest(n)
+        .map(|x| x.0)
+        .sum::<T>())
+}
+
+#[allow(dead_code)]
+fn count_calories_orig(puzzle_lines: &[String], n: usize) -> Result<u64, Box<dyn Error>> {
     let mut data = vec![];
     let mut total = 0;
 
@@ -36,9 +53,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // ==============================================================
 
-    let calories = count_calories(&puzzle_lines, 1)?;
+    let calories = count_calories::<u64>(&puzzle_lines, 1)?;
     writeln!(stdout, "Answer Part 1 = {calories}")?;
-    let calories = count_calories(&puzzle_lines, 3)?;
+    let calories = count_calories::<u64>(&puzzle_lines, 3)?;
     writeln!(stdout, "Answer Part 2 = {calories}")?;
 
     if args.get_flag("time") {
@@ -59,28 +76,32 @@ mod tests {
     #[test]
     fn part1_example() -> Result<(), Box<dyn Error>> {
         let puzzle_lines = get_data("input-example");
-        assert_eq!(count_calories(&puzzle_lines, 1)?, 24000);
+        assert_eq!(count_calories::<u64>(&puzzle_lines, 1)?, 24000);
+        assert_eq!(count_calories::<u64>(&puzzle_lines, 1)?, count_calories_orig(&puzzle_lines, 1)?);
         Ok(())
     }
 
     #[test]
     fn part1_actual() -> Result<(), Box<dyn Error>> {
         let puzzle_lines = get_data("input-actual");
-        assert_eq!(count_calories(&puzzle_lines, 1)?, 68467);
+        assert_eq!(count_calories::<u64>(&puzzle_lines, 1)?, 68467);
+        assert_eq!(count_calories::<u64>(&puzzle_lines, 1)?, count_calories_orig(&puzzle_lines, 1)?);
         Ok(())
     }
 
     #[test]
     fn part2_example() -> Result<(), Box<dyn Error>> {
         let puzzle_lines = get_data("input-example");
-        assert_eq!(count_calories(&puzzle_lines, 3)?, 45000);
+        assert_eq!(count_calories::<u64>(&puzzle_lines, 3)?, 45000);
+        assert_eq!(count_calories::<u64>(&puzzle_lines, 3)?, count_calories_orig(&puzzle_lines, 3)?);
         Ok(())
     }
 
     #[test]
     fn part2_actual() -> Result<(), Box<dyn Error>> {
         let puzzle_lines = get_data("input-actual");
-        assert_eq!(count_calories(&puzzle_lines, 3)?, 203420);
+        assert_eq!(count_calories::<u64>(&puzzle_lines, 3)?, 203420);
+        assert_eq!(count_calories::<u64>(&puzzle_lines, 3)?, count_calories_orig(&puzzle_lines, 3)?);
         Ok(())
     }
 }

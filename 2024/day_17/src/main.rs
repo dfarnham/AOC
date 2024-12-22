87f371bee4b26ea0cd6796be30cf6148da0c395b
Dave Farnham <dave.farnham@icloud.com>
2024-12-22 10:36:34 -0700

    cleanup

--- 2024/day_17/src/main.rs
+++ 2024/day_17/src/main.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 use std::io::{self, Write};
 
 fn get_program(data: &[String]) -> Result<(Vec<usize>, Vec<usize>), Box<dyn Error>> {
-    let mut register = vec![];
+    let mut registers = vec![];
     let mut program = vec![];
 
     for line in data {
@@ -13,109 +13,106 @@ fn get_program(data: &[String]) -> Result<(Vec<usize>, Vec<usize>), Box<dyn Erro
         }
         let field = &trim_split_on::<String>(line, ':')?[1];
         if line.starts_with("Register") {
-            register.push(field.parse::<usize>()?);
+            registers.push(field.parse::<usize>()?);
         } else if line.starts_with("Program") {
             program = trim_split_on::<usize>(field, ',')?;
         }
     }
-    Ok((register, program))
+    Ok((registers, program))
 }
 
-fn run_program(program: &[usize], register: &mut [usize]) -> Vec<usize> {
+fn run_program(program: &[usize], registers: &mut [usize]) -> Vec<usize> {
     let mut pc = 0;
     let mut output = vec![];
 
-    while pc < program.len() {
-        let mut jump = false;
-        let opcode = program[pc];
-        let operand = program[pc + 1];
+    while pc < program.len() - 1 {
+        let mut jmp = false;
+        let (opcode, operand) = (program[pc], program[pc + 1]);
         let combo_operand = match operand {
             n if [0, 1, 2, 3].contains(&n) => n,
-            4 => register[0],
-            5 => register[1],
-            6 => register[2],
+            4 => registers[0],
+            5 => registers[1],
+            6 => registers[2],
             _ => unreachable!(),
         };
         //println!("opcode {opcode}, operand {operand}, combo_operand {combo_operand}");
 
         match opcode {
             // adv
-            0 => register[0] /= 2_usize.pow(combo_operand as u32),
+            0 => registers[0] >>= combo_operand,
             // bxl
-            1 => register[1] ^= operand,
+            1 => registers[1] ^= operand,
             // bst
-            2 => register[1] = combo_operand % 8,
+            2 => registers[1] = combo_operand & 0b111,
             // jnz
             3 => {
-                if register[0] != 0 {
+                if registers[0] != 0 {
                     pc = operand;
-                    jump = true;
+                    jmp = true;
                 }
             }
             // bxc
-            4 => register[1] ^= register[2],
+            4 => registers[1] ^= registers[2],
             // out
-            5 => output.push(combo_operand % 8),
+            5 => output.push(combo_operand & 0b111),
             // bdv
-            6 => register[1] = register[0] / 2_usize.pow(combo_operand as u32),
+            6 => registers[1] = registers[0] >> combo_operand,
             // cdv
-            7 => register[2] = register[0] / 2_usize.pow(combo_operand as u32),
+            7 => registers[2] = registers[0] >> combo_operand,
             _ => unreachable!(),
         }
-        if !jump {
+        if !jmp {
             pc += 2
         }
     }
     output
 }
 
-fn dfs(program: &[usize], d: usize, v: usize) -> usize {
+#[allow(dead_code)]
+fn _dfs(program: &[usize], d: usize, n: usize) -> usize {
     let mut best = usize::MAX;
-    let mut register = vec![v, 0, 0];
-    if d == 0 || run_program(program, &mut register) == program[program.len() - d..].to_vec() {
+    if d == 0 || run_program(program, &mut [n, 0, 0]) == program[program.len() - d..] {
         if d == program.len() {
-            return v;
-        } else {
-            for i in 0..8 {
-                best = best.min(dfs(program, d + 1, v << 3 | i))
-            }
+            return n;
+        }
+        for i in 0..8 {
+            best = best.min(_dfs(program, d + 1, n << 3 | i))
         }
     }
     best
+}
+#[allow(dead_code)]
+fn dfs(program: &[usize]) -> usize {
+    _dfs(program, 0, 0)
 }
 
 fn priorityq(program: &[usize]) -> usize {
-    let mut best = usize::MAX;
     let mut workq = VecDeque::new();
     workq.push_back((0, 0));
-    while let Some((d, v)) = workq.pop_front() {
-        let mut register = vec![v, 0, 0];
-        if d == 0 || run_program(program, &mut register) == program[program.len() - d..].to_vec() {
+    while let Some((d, n)) = workq.pop_front() {
+        if d == 0 || run_program(program, &mut [n, 0, 0]) == program[program.len() - d..] {
             if d == program.len() {
-                best = v;
-                break;
+                return n;
             }
             for i in 0..8 {
-                workq.push_back((d + 1, v << 3 | i));
+                workq.push_back((d + 1, n << 3 | i));
             }
         }
     }
-    best
+    usize::MAX
 }
 
 fn solve(puzzle_lines: &[String], part2: bool) -> Result<String, Box<dyn Error>> {
-    let (mut register, program) = get_program(puzzle_lines)?;
+    let (mut registers, program) = get_program(puzzle_lines)?;
     if !part2 {
-        Ok(run_program(&program, &mut register)
+        Ok(run_program(&program, &mut registers)
             .iter()
             .map(|n| n.to_string())
             .collect::<Vec<_>>()
-            .join(",")
-            .to_string())
+            .join(","))
     } else {
-        let best = dfs(&program, 0, 0);
-        assert_eq!(best, priorityq(&program));
-        Ok(best.to_string())
+        //assert_eq!(dfs(&program), priorityq(&program));
+        Ok(priorityq(&program).to_string())
     }
 }
 
